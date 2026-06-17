35feb4b5d3a7337d1c7c9212821d3024e687abb1
everletter-x <everletter-x@users.noreply.github.com>
2026-06-18 00:10:12 +0700

    fix: tailwind config broken backgroundImage string

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,7 +22,7 @@ module.exports = {
         'starlight-glow': '#FFF8DC',
       },
       backgroundImage: {
-        'glassmorphism': 'url("data:image/svg+xml,%3Csvg viewBox='0 0 200 200' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noiseFilter'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.65' numOctaves='3' stitchTiles='stitch'/%3E%3C/filter%3E%3C/svg%3E")',
+        'glassmorphism': 'none',
       }
     },
   },
